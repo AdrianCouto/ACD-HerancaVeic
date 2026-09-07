0bef21efef51708ab8f700db80b26fe40a68167d
AdrianCouto <adrcm01@outlook.com>
2026-09-07 15:43:01 -0300

    Initialize Main class with ArrayList of Veiculos

---
+++ HerancaVeiculos/Main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;    
+
+public class Main{
+
+    public static void main(String[] args){
+
+        ArrayList<Veiculos> veiculos = new ArrayLista<>();
+
+        Scanner qtd = new Scanner(System.in);
+
+
+
+    }
+
+}
